9d2e639b99af08e696b1d55b192bdbb4a3848ab2
Md. Mehedi Hasan Tuhin <58122717+mhtuhinlrc@users.noreply.github.com>
2021-07-31 13:42:26 +0600

    Add files via upload

---
+++ binary to decimal.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+int main()
+{
+    char bin[32]={0};
+    int dec,i;
+    int cnt;
+    printf("enter binary value:");
+    gets(bin);
+    cnt=0;
+    dec=0;
+    for(i=(strlen(bin)-1;i>=0;i--);
+    {
+        dec=dec+(bin[i])
+    }
+}
